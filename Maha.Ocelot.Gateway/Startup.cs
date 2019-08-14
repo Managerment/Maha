bf57a04f2252b72cbf70f288e0d5ed7270049420
chensheng <504433707@qq.com>
2019-08-14 11:20:40 +0800

    123123

--- Maha.Ocelot.Gateway/Startup.cs
+++ Maha.Ocelot.Gateway/Startup.cs
@@ -5,6 +5,7 @@ using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Ocelot.DependencyInjection;
 using Ocelot.Middleware;
+using Ocelot.Provider.Consul;
 
 namespace Maha.Ocelot.Gateway
 {
@@ -20,7 +21,7 @@ namespace Maha.Ocelot.Gateway
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
-            services.AddOcelot();//注入Ocelot服务
+            services.AddOcelot().AddConsul();//注入Ocelot服务,//注入Consul服务
             services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_1);
         }
 
